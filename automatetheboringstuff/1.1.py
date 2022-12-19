233070f06429ca1ec2f55233e8fe9ac02c0f7ebf
Fidget <github@berry.st>
2022-12-19 13:13:18 -0800

    begin automate the boring stuff

---
+++ automatetheboringstuff/1.1.py
@@ -0,0 +1,13 @@
+# This program says hello and asks for my name.
+
+print('Hello, world!')
+
+name = input('What is your name? ')
+
+print(f'It is nice to meet you, {name}.')
+
+print(f'The length of your name is {len(name)}.')
+
+age = input('What is your age? ')
+
+print(f'You will be {int(age)+1} in a year!')
---
+++ automatetheboringstuff/readme.md
@@ -0,0 +1,5 @@
+# Automate the Boring Stuff
+
+https://automatetheboringstuff.com/#toc
+
+Directory for practicing problems in automate the boring stuff.
